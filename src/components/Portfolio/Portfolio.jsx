8882fcdd9e18e3d9b6735ecf8ffdf170f8b133b6
Kevin Ayala <kevinayala6969@gmail.com>
2021-09-10 20:54:22 -0700

    adds screenshots and links for projects

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -4,7 +4,30 @@ import "./Portfolio.scss"
 export default function Portfolio() {
     return (
         <div className="portfolio" id="portfolio">
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li className="active">Featured</li>
+                <li>Web App</li>
+                <li>Games</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="Assets/portfolio1.png" alt="FC Board (Web App)" />
+                    <h3>Final Fantasy 14: Free Company Finder</h3>
+                </div>
+                <div className="item">
+                    <img src="Assets/portfolio1.png" alt="FC Board (Web App)" />
+                    <h3>Final Fantasy 14: Free Company Finder</h3>
+                </div>
+                <div className="item">
+                    <img src="Assets/portfolio1.png" alt="FC Board (Web App)" />
+                    <h3>Final Fantasy 14: Free Company Finder</h3>
+                </div>
+                <div className="item">
+                    <img src="Assets/portfolio1.png" alt="FC Board (Web App)" />
+                    <h3>Final Fantasy 14: Free Company Finder</h3>
+                </div>
+            </div>
         </div>
     )
 }
