3b2e7589e9fb6f51b741008f80b440199034bab2
Kevin Ayala <kevinayala6969@gmail.com>
2021-09-10 20:19:21 -0700

    adds intro animation and image

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,10 +1,36 @@
-import React from 'react'
+import {React, useEffect, useRef} from 'react'
 import "./Intro.scss"
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import { init } from 'ityped'
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(()=>{
+        init(textRef.current, {
+            showCursor: true,
+            backDelay: 1500,
+            strings: ["Developer", "Designer", "Engineer"]
+        });
+    },[]);
     return (
         <div className="intro" id="intro">
-            Hello my name is Kevin
+            <div className="left">
+                <div className="imgContainer">
+                    <img src="Assets/avatar.png" alt="Developer avatar icon" />
+                </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Welcome!, I'm</h2>
+                    <h1>Kevin Ayala</h1>
+                    <h3>Software <span ref={textRef}></span></h3>
+                </div>
+                <a href="#portfolio">
+                    <ExpandMoreIcon className="expandIcon"/>
+                </a>
+            </div>
         </div>
     )
 }
--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import "./Topbar.scss"
 import {Person, Mail} from "@material-ui/icons"
 
