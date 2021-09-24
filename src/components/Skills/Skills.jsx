f5ec3b24f500708f0e96179eea4c0eed6432249d
Kevin Ayala <kevinayala6969@gmail.com>
2021-09-24 09:57:07 -0700

    adds all skill icons with animation

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import "./Skills.scss"
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
 
 export default function Skills() {
 
@@ -9,6 +8,42 @@ export default function Skills() {
             name: "HTML",
             icon: <i className="user" class="devicon-html5-plain colored"></i>,
         },
+        {
+            name: "CSS3",
+            icon: <i className="user" class="devicon-css3-plain colored"></i>,
+        },
+        {
+            name: "JavaScript",
+            icon: <i className="user" class="devicon-javascript-plain colored"></i>,
+        },
+        {
+            name: "Python",
+            icon: <i className="user" class="devicon-python-plain colored"></i>,
+        },
+        {
+            name: "React",
+            icon: <i className="user" class="devicon-react-original colored"></i>,
+        },
+        {
+            name: "Django",
+            icon: <i className="user" class="devicon-django-plain colored"></i>,
+        },
+        {
+            name: "Node JS",
+            icon: <i className="user" class="devicon-nodejs-plain colored"></i>,
+        },
+        {
+            name: "Express JS",
+            icon: <i className="user" class="devicon-express-original colored"></i>,
+        },
+        {
+            name: "MongoDB",
+            icon: <i className="user" class="devicon-mongodb-plain colored"></i>,
+        },
+        {
+            name: "PostgreSQL",
+            icon: <i className="user" class="devicon-postgresql-plain colored"></i>,
+        },
     ]
     return (
         <div className="skills" id="skills">
@@ -16,7 +51,7 @@ export default function Skills() {
             <div className="container">
                 {data.map(d=>(
 
-                    <div className="card">
+                    <div className={d.featured ? "card featured" : "card"}>
                     <div className="top">
                         <div>{d.icon}</div>
                    </div>
